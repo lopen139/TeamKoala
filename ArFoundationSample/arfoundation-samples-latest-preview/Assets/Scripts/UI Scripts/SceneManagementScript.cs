c943319ddb0d3b65ad9a0feeec61b472a0dfa813
Rob Jacobs <R.T.J.M.Jacobs@students.uu.nl>
2020-11-06 15:37:54 +0100

    Finished story 1 scene

---
+++ ArFoundationSample/arfoundation-samples-latest-preview/Assets/Scripts/UI Scripts/SceneManagementScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagementScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void PreviousStory()
+    {
+        var scene = SceneManager.GetActiveScene().name;
+        if(string.Equals(scene, "Story 1", System.StringComparison.InvariantCultureIgnoreCase))
+        {
+
+        }
+        if (string.Equals(scene, "Story 2", System.StringComparison.InvariantCultureIgnoreCase))
+        {
+            SceneManager.LoadScene("Story 1");
+        }
+        if (string.Equals(scene, "Story 3", System.StringComparison.InvariantCultureIgnoreCase))
+        {
+            SceneManager.LoadScene("Story 2");
+        }
+    }
+
+    public void NextStory()
+    {
+        var scene = SceneManager.GetActiveScene();
+        if (string.Equals(scene, "Story 1", System.StringComparison.InvariantCultureIgnoreCase))
+        {
+            SceneManager.LoadScene("Story 2");
+        }
+        if (string.Equals(scene, "Story 2", System.StringComparison.InvariantCultureIgnoreCase))
+        {
+            SceneManager.LoadScene("Story 3");
+        }
+        if (string.Equals(scene, "Story 3", System.StringComparison.InvariantCultureIgnoreCase))
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
